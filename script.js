f1431b999aa14099b915a7329fee1bc30da059b2
Diego Dmitry <diegodmitry@hotmail.com>
2025-03-28 11:42:15 +0000

    added a file.

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ downloadBtn.addEventListener('click', function() {
   downloadBtn.disabled = true;
   downloadBtn.textContent = 'Downloading...';
 
-  // I used setTimeout function just to simulate a download process
+  // Create a link element
+  const link = document.createElement('a');
+  link.href = 'system_search_202503281128398916042148766458854.csv';
+  link.download = 'data.csv'; // Name that will appear when downloading
+
+  // Trigger the download
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  // Reset button state after a short delay
   setTimeout(function() {
-    // After the "download" is complete:
     downloadBtn.disabled = false;
     downloadBtn.textContent = 'Download';
-  }, 5000); // 5 seconds
+  }, 1000);
 });
